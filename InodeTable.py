303e465d14a184c26e9f50ae71215667d795ceb9
cefeboru <cefeboru@gmail.com>
2017-11-17 22:49:33 -0600

    Change File offset on each method

--- InodeTable.py
+++ InodeTable.py
@@ -1,5 +1,7 @@
-"Module to handle all the Inode Table Operations"
-import struct
+'''
+Author: Cesar Bonilla
+Module to handle all the Inode Table Operations
+'''
 from InodeBase import Inode
 from Settings import Settings
 from bitarray import bitarray
@@ -20,19 +22,20 @@ class InodeTable(object):
         '''
         Read the Inode with the specified id
         '''
-        offset = inode_id * Settings.inode_size
-        file_object.seek(offset, 1)
+        file_object.seek(Settings.inode_table_offset)
+        inode_offset = inode_id * Settings.inode_size
+        file_object.seek(inode_offset, 1)
         i_bytes = file_object.read(Settings.inode_size)
         inode = Inode().from_binary(i_bytes)
         return inode
 
     @classmethod
-    def get_first_free_inode(cls, file_object):
+    def get_free_inode_index(cls, file_object):
         '''
-        Reads and return the first inode element that us unused
+        Reads and return the first inode element that is free
         '''
+        file_object.seek(Settings.inode_bitmap_offset)
         bitmap_bytes = file_object.read(Settings.inode_bitmap_size)
-        print "Inode Bitmap"
         data = bitarray()
         data.frombytes(bitmap_bytes)
         try:
@@ -40,3 +43,10 @@ class InodeTable(object):
             return free_inode_position
         except ValueError:
             raise ValueError("No more free Inodes, please delete some files")
+
+    @classmethod
+    def set_inode_as_occupied(cls, inode_id):
+        '''
+        Stablish the inode as occupied, will set the bit to 0 in the bitmap
+        '''
+        raise ValueError("To be implemented @Gisselle")
