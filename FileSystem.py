75571a88d8ea82164a4a77ba89a8f68ab720b0a1
cefeboru <cefeboru@gmail.com>
2017-11-19 07:50:39 -0600

    We can read the files inside the root directory

--- FileSystem.py
+++ FileSystem.py
@@ -5,6 +5,9 @@ Module to Handle the Ext2 File System
 '''
 
 import struct
+import calendar
+import time
+import math
 from bitarray import bitarray
 from InodeBase import Inode
 from ClusterTable import ClusterTable
@@ -17,11 +20,14 @@ class FileSystem(object):
     and create files. All size properties are in bytes.
     '''
 
-    def __init__(self, fs_file):
+    def __init__(self, fs_file, create_fs=False):
         self.file_object = fs_file
         self.working_dir = "/"
         self.InodeTable = InodeTable(fs_file)
         self.ClusterTable = ClusterTable(fs_file)
+        self.__current_inode_id = 0
+        if(not create_fs):
+            self.__root_inode = self.InodeTable.get_root_inode()
 
     def _create_file_system(self):
         "Create a new ext2 file with the default structure"
@@ -64,37 +70,76 @@ class FileSystem(object):
         '''
         self.InodeTable.set_inode_as_occupied(0)
         self.ClusterTable.set_cluster_as_occupied(0)
-        root_inode = self.InodeTable.get_root_inode()
-        print "Root Inode cri: {0}".format(root_inode)
-        
-        #Creating dir entry
-        print "Allocating first block for root inode"
-        root_inode.i_size += self.__add_dir_entry("/", 0, 0)
-        self.InodeTable.set_inode(0, root_inode)
-        print "Allocated"
+        self.__root_inode = self.InodeTable.get_root_inode()
+        self.__current_inode_id = 0
+        #TODO ADD CURRENT DIRECTORY "."
 
-    def __add_dir_entry(self, file_name, inode_id, file_type):
+    def __add_dir_entry(self, file_name, inode_id, file_type, parent_inode_id):
         '''
-        Adds a new dir entry at the current directory and returns the dir entry size
+        Adds a new dir entry at the current directory and returns the dir entry size.
+        file_type: 0 => regular file, 1 => directory file
         '''
+        parent_inode = self.InodeTable.get_inode(parent_inode_id)
         name_len = len(file_name)
         struct_mask = '=hhhh{0}s'.format(name_len)
         dir_entry_size = struct.calcsize(struct_mask)
+        print "Adding entry {0},{1},{2},{3},{4}".format(inode_id, dir_entry_size, name_len, file_type, file_name)
         block_data = struct.pack(struct_mask, inode_id, dir_entry_size, name_len, file_type, file_name)
+        cluster_offset = int(math.ceil(parent_inode.i_size / Settings.datablock_size))
+        print "At block pointer {0}, with adress {1}".format(cluster_offset, parent_inode.i_blocks[cluster_offset])
+        if(cluster_offset > 1):
+            data_offset = parent_inode.i_size - Settings.datablock_size
+        else:
+            data_offset = parent_inode.i_size
+        data_offset = parent_inode.i_blocks[cluster_offset] + data_offset
+        print "And data offset {0}".format(data_offset)
+        #Move at start of Data Region
         self.file_object.seek(Settings.datablock_region_offset)
-        self.file_object.seek(Settings.inode_size * inode_id, 1)
+        #Move to the last occupied block
+        self.file_object.seek(data_offset, 1)
         self.file_object.write(block_data)
         return dir_entry_size
+
+    def read_file(self, file_name):
+        "Try to read the file_name: in the current directory"
+
+        print "READ {0}".format(file_name)
+        current_inode = self.InodeTable.get_inode(self.__current_inode_id)
+        data_size = current_inode.i_size
+        bytes_readed = 0
+        self.file_object.seek(Settings.datablock_region_offset)
+        entry_mask = "=hhhh"
+        while bytes_readed < data_size:
+            entry_size = struct.calcsize(entry_mask)
+            inode_id, rec_len, name_len, file_type = struct.unpack(
+                entry_mask, self.file_object.read(entry_size))
+            name_mask = "={0}s".format(name_len)
+            file_name = struct.unpack(name_mask, self.file_object.read(name_len))[0]
+            bytes_readed += struct.calcsize(entry_mask + name_mask[1:])
+            print "Dir Entry: {0},{1},{2},{3},{4}".format(inode_id, rec_len, name_len, file_type, file_name)
+
 
     def create_file(self, file_name):
         '''
         Creates a new file and return the assigned Inode ID
         '''
-        inode_id = -1
-        self.InodeTable.get_free_inode_index()
-        return inode_id
+        current_inode = self.InodeTable.get_inode(self.__current_inode_id)
+        child_inode_id = self.InodeTable.get_free_inode_index()
+        current_inode.i_size += self.__add_dir_entry(file_name, child_inode_id, 0, 0)
+        self.InodeTable.set_inode(self.__current_inode_id, current_inode)
+        self.InodeTable.set_inode_as_occupied(child_inode_id)
+        return child_inode_id
 
-
+    def create_folder(self, folder_name):
+        '''
+        Creates a new file and return the assigned Inode ID
+        '''
+        current_inode = self.InodeTable.get_inode(self.__current_inode_id)
+        child_inode_id = self.InodeTable.get_free_inode_index()
+        current_inode.i_size += self.__add_dir_entry(folder_name, child_inode_id, 1, 0)
+        self.InodeTable.set_inode(self.__current_inode_id, current_inode)
+        self.InodeTable.set_inode_as_occupied(child_inode_id)
+        return child_inode_id
 
     @classmethod
     def bits(cls, f):
--- Main.py
+++ Main.py
@@ -3,19 +3,20 @@
 """
 import os
 from FileSystem import FileSystem
-from Settings import Settings
-from InodeTable import InodeTable
-from ClusterTable import ClusterTable
 
-with open("FS_test.ext2", "r+b") as fs_file:
-    file_system = FileSystem(fs_file)
-    file_system._create_file_system()
+new_fs = False
+file_path = "FS_test.ext2"
+if not os.path.isfile(file_path):
+    new_fs = True
+    open(file_path, "wb").close()
 
-with open("FS_test.ext2", "r+b") as fs_file:
-    file_system = FileSystem(fs_file)
+with open(file_path, "r+b") as fs_file:
+    file_system = FileSystem(fs_file, new_fs)
+    if(new_fs):
+        file_system._create_file_system()
     #Test InodeTable > Get Root Inode
-    root_inode = file_system.InodeTable.get_inode(1)
-    print "Root Inode: {0}".format(root_inode)
+    root_inode = file_system.InodeTable.get_inode(0)
+    print root_inode
     #Testing InodeTable > Get Free Inode
     free_inode = file_system.InodeTable.get_free_inode_index()
     print "First free inode is in index {0}".format(free_inode)
@@ -23,3 +24,8 @@ with open("FS_test.ext2", "r+b") as fs_file:
     free_cluster, cluster_offset = file_system.ClusterTable.get_next_free_cluster()
     print "Free Cluster should be 1, is {0}".format(free_cluster)
     print "Free Cluster offset is {0}".format(cluster_offset)
+    file_system.read_file("/")
+    print "Creating new file"
+    created_inode = file_system.create_file("Cesar.txt")
+    print "Created inode {0}".format(created_inode)
+    file_system.read_file("/")
