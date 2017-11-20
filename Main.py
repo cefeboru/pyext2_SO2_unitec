62a9ad3ca966fbd36f390c786c812c9e374a2f9c
cefeboru <cefeboru@gmail.com>
2017-11-20 16:40:25 -0600

    Creates FileSystem fixed when no file found

--- Main.py
+++ Main.py
@@ -6,18 +6,16 @@ import sys
 from FileSystem import FileSystem
 
 new_fs = False
-file_path = "FS_test.ext2"
+file_path = "FS.ext2"
 if not os.path.isfile(file_path):
     new_fs = True
     open(file_path, "wb").close()
 
 with open(file_path, "r+b") as fs_file:
-    
     file_system = FileSystem(fs_file, new_fs)
-    file_system.write_file("test.txt", "Test")
-    """
     if new_fs:
-    file_system._create_file_system()
+        file_system._create_file_system()
+    """
     file_system.write_file("cesar.txt", "Hello World")
     file_system.read_file("cesar.txt")
     file_system.create_directory("test")
